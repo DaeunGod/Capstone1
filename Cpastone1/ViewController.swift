93f267303865e586f22b6dfc047a338e60f12bd0
DaeunGod <dcj4655@naver.com>
2018-03-23 14:44:03 +0900

    add message array

--- Cpastone1/ViewController.swift
+++ Cpastone1/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let messageArray = ["May the force be with you", "Live long and prosper", "To inifinity and beyond", "Space is big. You just won't believe how vastly, hugely, mindbogglinly big it is", 10] as [Any]
+    var index = 0
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        messageLabel.text = messageArray[index] as? String
+        messageLabel.adjustsFontSizeToFitWidth = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonPress(_ sender: UIButton) {
+        
+        index = (index+1)%(messageArray.count)
+        
+        if let nextString = messageArray[index] as? String {
+            messageLabel.text = nextString
+        }
+               
+    }
+    
 }
 
